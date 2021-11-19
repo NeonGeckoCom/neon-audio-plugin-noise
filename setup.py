78cdfed8439fd6cdd99a71d8b427fb397249cbd7
jarbasai <jarbasai@mailfence.com>
2021-11-19 06:44:59 +0000

    convention neon_audio_xxx_plugin

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup
 
 
-PLUGIN_ENTRY_POINT = 'neon_noise_level_plugin=neon_noise_level_plugin:BackgroundNoise'
+PLUGIN_ENTRY_POINT = 'neon_audio_noise_plugin=neon_audio_noise_plugin:BackgroundNoise'
 setup(
-    name='neon_noise_level_plugin',
+    name='neon_audio_noise_plugin',
     version='0.0.1',
     description='A audio parser/classifier/transformer plugin for ovos/neon/mycroft',
     url='https://github.com/NeonGeckoCom/neon_noise_level_plugin',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='bsd3',
-    packages=['neon_noise_level_plugin'],
+    packages=['neon_audio_noise_plugin'],
     zip_safe=True,
     keywords='mycroft plugin audio parser/classifier/transformer',
     entry_points={'neon.plugin.audio': PLUGIN_ENTRY_POINT}
