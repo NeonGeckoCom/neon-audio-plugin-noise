89c3436dda89c0ff7e606d62d902b665f899e204
NeonDmitry <dmitry@neon.ai>
2022-12-20 20:33:00 +0200

    added unittest

--- neon_audio_noise_plugin/__init__.py
+++ neon_audio_noise_plugin/__init__.py
@@ -10,6 +10,7 @@
 #    and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
 #    products derived from this software without specific prior written permission.
+import wave
 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 # INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -52,4 +53,3 @@ class BackgroundNoise(AudioTransformer):
 
 def create_module(config=None):
     return BackgroundNoise(config=config)
-
